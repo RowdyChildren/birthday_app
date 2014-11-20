0d4b76a00d12443ba3139de6b195c140ef196f14
Riley Childs <riley@tfsgeo.com>
2014-11-19 20:50:58 -0500

    Finsihed up

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -3,5 +3,24 @@ class Owner
 	def name
 		name = "Riley"
 	end
+	
+	def birthdate
+		birthdate = Date.new(1997, 11, 19)
+	end
+	
+	def countdown
+		
+		today = Date.today
 
+		birthday = Date.new(today.year, birthdate.month , birthdate.day)
+
+		if birthday > today
+			countdown = (birthday - today).to_i
+		elsif birthday < today
+			countdown = (birthday.next_year - today).to_i
+		elsif birthday == today
+			return "Happy Birthday"
+		end
+
+	end
 end
